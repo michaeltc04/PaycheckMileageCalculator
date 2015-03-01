c22208c610535c4b055308d7fe9b0cf626962765
Wilson Burhan <wilson.burhan@gmail.com>
2015-02-28 20:19:52 -0800

    Initial commit

---
+++ app/src/main/java/com/michaelt/paycheckmileagecalculator/HourlyFragment.java
@@ -0,0 +1,32 @@
+package com.michaelt.paycheckmileagecalculator;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+public class HourlyFragment extends Fragment {
+    private View mView;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        mView = inflater.inflate(R.layout.fragment_hourly, null);
+
+        Spinner spinner = (Spinner) mView.findViewById(R.id.filing_status_spinner);
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getActivity().getApplicationContext(),
+                R.array.filing_status_array, android.R.layout.simple_spinner_item);
+        // Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
+
+
+        return mView;
+    }
+
+}
